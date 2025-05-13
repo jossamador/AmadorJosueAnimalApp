e8ca1a5a98bf925bbd22eb6f8bd3546958c237a2
jossamador <jos-amador@hotmail.com>
2025-05-13 11:12:28 -0600

    Commit 24, se agregó una lista de hechos del ambiente

--- app/src/main/java/com/example/amadorjosueanimalapp/model/Ambiente.kt
+++ app/src/main/java/com/example/amadorjosueanimalapp/model/Ambiente.kt
@@ -1,8 +1,10 @@
+// Ambiente.kt
 package com.example.amadorjosueanimalapp.model
 
 data class Ambiente(
     val id: String,
     val name: String,
     val description: String,
-    val image: String
+    val image: String,
+    val facts: List<String> = emptyList() // Hechos
 )
